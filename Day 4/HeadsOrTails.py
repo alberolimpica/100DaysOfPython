7facd5046c3b8d2e4a29290643671d4f8457f0d8
Sara Alberola Mahiques <alberolimpica@users.noreply.github.com>
2021-02-28 12:08:54 +0100

    Create HeadsOrTails.py

---
+++ Day 4/HeadsOrTails.py
@@ -0,0 +1,9 @@
+#In this exercise we practice the use of the library random
+import random
+
+heads_or_tails = random.randint(0,1)
+
+if heads_or_tails == 1:
+  print("Heads")
+else:
+  print("Tails")
