c95316f27eb0363c6a3d6c7b4b0ecfb368809a4d
Sara Alberola Mahiques <alberolimpica@users.noreply.github.com>
2021-02-28 12:23:33 +0100

    Create LoveCalculator.py

---
+++ Day 3/LoveCalculator.py
@@ -0,0 +1,26 @@
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+names = name1 + name2
+names_to_lower_case = names.lower()
+#True
+true_result = names_to_lower_case.count('t')
+true_result += names_to_lower_case.count('r')
+true_result += names_to_lower_case.count('u')
+true_result += names_to_lower_case.count('e')
+
+#Love
+love_result = names_to_lower_case.count('l')
+love_result += names_to_lower_case.count('o')
+love_result += names_to_lower_case.count('v')
+love_result += names_to_lower_case.count('e')
+
+result = int(str(true_result) + str(love_result))
+
+if result < 10 or result > 90:
+  print(f"Your score is {result}, you go together like coke and mentos.")
+elif result >= 40 and result <= 50:
+  print(f"Your score is {result}, you are alright together.")
+else:
+  print(f"Your score is {result}")
