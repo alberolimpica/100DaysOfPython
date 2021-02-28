2941180d0d4bf67b946edd25849a9e5a7441330c
Sara Alberola Mahiques <alberolimpica@users.noreply.github.com>
2021-02-28 12:14:55 +0100

    Update PrintingExercise.py

--- Day 1/PrintingExercise.py
+++ Day 1/PrintingExercise.py
@@ -1,4 +1,18 @@
-#In this lesson we practice how to print in Python
+#In this lesson we practice how to print and get data in Python. We also learnt about data types and some basic functions, like how to stringify a number,
+#or what's the length of a string, how to deal with variables and switching its values
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("There are " + str(len(input("What's your name? "))) + " letters in your name")
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+a = input("a: ")
+b = input("b: ")
+
+aux = a
+a = b
+b = aux 
